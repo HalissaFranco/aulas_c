70ace0b1869e85f87ef8ffee49d0c8a9a7e4d0b0
Likah75 <likahcode@gmail.com>
2022-04-20 20:03:54 -0300

    aula 01

---
+++ exemplosPrint.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int inteiro = 6;
+    float pontoFlutuante = 2.567;
+    char texto[10] = "Ola mundo!";
+
+    printf("Int: %d\n", inteiro);
+    printf("Float: %f\n", pontoFlutuante);
+    printf("String: %s\n", texto);
+
+    return 0;
+}
