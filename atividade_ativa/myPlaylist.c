381a9451f8f271c1583f874960054d1b9e14ed54
Likah75 <likahcode@gmail.com>
2022-04-22 22:28:37 -0300

    Atividade Ativa

--- atividade_ativa/myPlaylist.c
+++ atividade_ativa/myPlaylist.c
@@ -9,7 +9,37 @@
 
 
 int main(void)
-{
+{   
+    /*Estrutura que armazena uma música*/
+    struct musica{
+        char nome[20];
+        char autor[20];
+        int duracao;
+    };
+
+    int op;
+
+
+    op = 1;
+    while (op !=0)
+    {
+        switch (op)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 0:
+            break;
+                        
+        default:
+            break;
+        }
+    }
+    
+
+
+
     printf("Em implementação :)\n");
 
     return 0;
